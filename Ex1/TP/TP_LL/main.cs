401d82fab00575f2e53b96a5587f7f1bc562e107
r00bertos1 <69626763+r00bertos1@users.noreply.github.com>
2020-11-15 15:58:32 +0100

    added data files

--- Ex1/TP/TP_LL/main.cs
+++ Ex1/TP/TP_LL/main.cs
@@ -14,15 +14,51 @@ namespace TP_LL
     {
         public static void Main()
         {
-            string fileName = (@"..\..\..\..\data.json");
+/*            string fileName = (@"..\..\..\..\data.json");
+
+            *//*            Client c1 = new Client("Andrzej", "Duda", 127301025);
+                        var json = new JavaScriptSerializer().Serialize(c1);
+                        //Console.WriteLine(json);
+
+                        DataService ds = new DataService(new DataRepository());
+
+                        ds.AddClient("Andrzej", "Duda", 127301025);
+
+                        DataContext dc = new DataContext();
+                        dc.Clients.Add(c1);
+
+                        var json2 = new JavaScriptSerializer().Serialize(dc);
+                        Console.WriteLine(json2);*//*
 
             using (StreamReader r = new StreamReader(fileName))
             {
                 string json = r.ReadToEnd();
-                List<Client> items = JsonConvert.DeserializeObject<List<Client>>(json);
-                string Something = string.Join(",", items);
-                Console.Write(Something);
-            }
+                *//*                DataContext dc = new DataContext();
+
+                                dynamic ddc = JsonConvert.DeserializeObject(json);
+                                string Something = string.Join(",", ddc);
+                                Console.Write(Something);*//*
+                DataContext dc = new DataContext();
+                dc = JsonConvert.DeserializeObject<DataContext>(json);
+                //List<Client> clients = new List<Client>();
+
+                foreach (var item in dc.Clients)
+                {
+                    Console.WriteLine("firstName: {0}, lastName: {1}, phoneNumber: {2}, id: {3}", item.FirstName, item.LastName, item.PhoneNumber, item.Id);
+                }
+                foreach (var item in dc.Cars)
+                {
+                    //Console.WriteLine("firstName: {0}, lastName: {1}, phoneNumber: {2}, id: {3}", Client.FirstName, Client.LastName, Client.PhoneNumber, item.Id);
+                }
+                foreach (var item in dc.Factures)
+                {
+                    //Console.WriteLine("firstName: {0}, lastName: {1}, phoneNumber: {2}, id: {3}", Client.FirstName, Client.LastName, Client.PhoneNumber, Client.Id);
+                }
+                foreach (var item in dc.WarehouseItems)
+                {
+                    //Console.WriteLine("firstName: {0}, lastName: {1}, phoneNumber: {2}, id: {3}", Client.FirstName, Client.LastName, Client.PhoneNumber, Client.Id);
+                }
+            }*/
         }
     }
 }
--- Ex1/TP/TP_UnitTests/DataInsert.cs
+++ Ex1/TP/TP_UnitTests/DataInsert.cs
@@ -1,16 +1,55 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using TP;
+using TP.Objects;
 
 namespace TP_UnitTests
-
 {
     class DataInsert : IDataFiller
     {
         public void InsertData(DataContext dataContext)
         {
-            throw new NotImplementedException();
+            string fileName = (@"..\..\..\..\data.json");
+
+            /*            Client c1 = new Client("Andrzej", "Duda", 127301025);
+                        var json = new JavaScriptSerializer().Serialize(c1);
+                        //Console.WriteLine(json);
+
+                        DataService ds = new DataService(new DataRepository());
+
+                        ds.AddClient("Andrzej", "Duda", 127301025);
+
+                        DataContext dc = new DataContext();
+                        dc.Clients.Add(c1);
+
+                        var json2 = new JavaScriptSerializer().Serialize(dc);
+                        Console.WriteLine(json2);*/
+
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                string json = r.ReadToEnd();
+                /*                DataContext dc = new DataContext();
+
+                                dynamic ddc = JsonConvert.DeserializeObject(json);
+                                string Something = string.Join(",", ddc);
+                                Console.Write(Something);*/
+                dataContext = JsonConvert.DeserializeObject<DataContext>(json);
+                List<Client> clients = new List<Client>();
+                List<Car> cars = new List<Car>();
+                List<Facture> factures = new List<Facture>();
+                List<WarehouseItem> warehouseItems = new List<WarehouseItem>();
+
+                foreach (var Client in dataContext.Clients)
+                {
+                    Console.WriteLine("firstName: {0}, lastName: {1}, phoneNumber: {2}, id: {3}", Client.FirstName, Client.LastName, Client.PhoneNumber, Client.Id);
+                }
+
+            }
+
+
         }
     }
 }
