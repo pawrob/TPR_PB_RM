2fac0d46e8d2f67660c686d8323c1905604ab54d
Paweł Bucki <mrpawrob@gmail.com>
2021-01-02 16:38:00 +0100

    ADDED [DB connection, tests]

---
+++ Ex3/TP_LINQ/DataService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_LINQ
+{
+    public class DataService
+    {
+        private static DataClassesDataContext dataContext = new DataClassesDataContext();
+
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            Table<Product> products = dataContext.GetTable<Product>();
+            List<Product> result = (from product in products
+                                    where product.Name.Contains(namePart)
+                                    select product).ToList();
+
+            return result;
+        }
+
+        public static List<Product> GetProductByVendorName(string vendorName)
+        {
+            Table<ProductVendor> productVendors = dataContext.GetTable<ProductVendor>();
+            List<Product> result = (from productVendor in productVendors
+                                    where productVendor.Vendor.Name.Equals(vendorName)
+                                    select productVendor.Product).ToList();
+
+            return result;
+        }
+
+        public static List<string> GetProductNamesByVendorName(string vendorName)
+        {
+
+            Table<ProductVendor> productVendors = dataContext.GetTable<ProductVendor>();
+            List<string> result = (from productVendor in productVendors
+                                   where productVendor.Vendor.Name.Equals(vendorName)
+                                   select productVendor.Product.Name).ToList();
+
+            return result;
+        }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            Table<ProductVendor> productVendors = dataContext.GetTable<ProductVendor>();
+            string result = (from productVendor in productVendors
+                             where productVendor.Product.Name.Equals(productName)
+                             select productVendor.Vendor.Name).First();
+
+            return result;
+        }
+    }
+
+
+
+}
---
+++ Ex3/TP_LINQ_TEST/AWQuerryTests.cs
@@ -0,0 +1,49 @@
+﻿using TP_LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace TP_LINQ_TEST
+{
+    [TestClass]
+    public class AWQuerryTests
+    {
+        [TestMethod]
+        public void GetProductsByNameTest()
+        {
+            List<Product> p1 = DataService.GetProductsByName("Blade");
+            List<Product> p2 = DataService.GetProductsByName("Flat Washer");
+            List<Product> p3 = DataService.GetProductsByName("2137");
+            Assert.AreEqual(1, p1.Count);
+            Assert.AreEqual(9, p2.Count);
+            Assert.AreEqual(0, p3.Count);
+        }
+
+        [TestMethod]
+        public void GetProductByVendorNameTest()
+        {
+            List<Product> p1 = DataService.GetProductByVendorName("Vista Road Bikes");
+            List<Product> p2 = DataService.GetProductByVendorName("");
+            Assert.AreEqual(2, p1.Count);
+            Assert.AreEqual(0, p2.Count);
+        }
+
+        [TestMethod]
+        public void GetProductNamesByVendorNameTest()
+        {
+            List<string> p1 = DataService.GetProductNamesByVendorName("Vista Road Bikes");
+            List<string> p2 = DataService.GetProductNamesByVendorName("");
+            Assert.AreEqual(2, p1.Count);
+            Assert.AreEqual(0, p2.Count);
+        }
+
+        [TestMethod]
+        public void GetProductVendorByProductNameTest()
+        {
+            string vendorName = "Continental Pro Cycles";
+            string v1 = DataService.GetProductVendorByProductName("Flat Washer 1");
+            Assert.AreEqual(vendorName, v1);
+        }
+    }
+}
