dc8c9ca447c458571437480e2d3b201fdd8cdc5b
r00bertos1 <69626763+r00bertos1@users.noreply.github.com>
2021-01-23 20:31:37 +0100

    ADDED [ViewModel and View]

---
+++ Ex4/TP_WPF/ModelView/Command.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action m_action;
+
+        public Command(Action action)
+        {
+            this.m_action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Task.Run(() =>
+            {
+                this.m_action();
+            });
+        }
+    }
+}
--- Ex4/TP_WPF/ModelView/MainViewModel.cs
+++ Ex4/TP_WPF/ModelView/MainViewModel.cs
@@ -1,10 +1,103 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Text;
 
 namespace ViewModel
 {
-    class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DataContext m_dataContext;
+        private ObservableCollection<MyProduct> m_Products;
+        private MyProduct m_CurrentProduct;
+        private string m_CurrentMessage;
+
+
+        public MainViewModel()
+        {
+            m_dataContext = new DataContext();
+            m_Products = m_dataContext.products;
+            m_CurrentProduct = m_Products[0];
+            m_CurrentMessage = "Welcome";
+            AddSampleProduct = new Command(() => { CurrentMessage = m_dataContext.addProduct(new MyProduct(21, "Product", "JP-2137", "Purple", 100, 2.04m, "big", 21.15m)); Products = m_dataContext.products; });
+            RemoveSampleProduct = new Command(() => { CurrentMessage = m_dataContext.removeProduct("test"); Products = m_dataContext.products; });
+            UpdateSampleProduct = new Command(() => { CurrentMessage = m_dataContext.updateProduct(CurrentProduct); Products = m_dataContext.products; });
+        }
+        public MainViewModel(DataContext myDataContext)
+        {
+            m_dataContext = myDataContext;
+            m_Products = m_dataContext.products;
+            m_CurrentProduct = m_Products[0];
+            m_CurrentMessage = "Welcome";
+
+            AddSampleProduct = new Command(() => { CurrentMessage = m_dataContext.addProduct(new MyProduct(21, "Product", "JP-2137", "Purple", 100, 2.04m, "big", 21.15m)); Products = m_dataContext.products; });
+            RemoveSampleProduct = new Command(() => { CurrentMessage = m_dataContext.removeProduct("test"); Products = m_dataContext.products; });
+            UpdateSampleProduct = new Command(() => { CurrentMessage = m_dataContext.updateProduct(CurrentProduct); Products = m_dataContext.products; });
+        }
+
+
+        public MyProduct EditedProduct
+        {
+            get => m_CurrentProduct;
+        }
+
+        public ObservableCollection<MyProduct> Products
+        {
+            get => m_Products;
+            set
+            {
+                m_Products = value;
+                onPropertyChanged(nameof(Products));
+            }
+        }
+        public MyProduct CurrentProduct
+        {
+            get => m_CurrentProduct;
+            set
+            {
+                m_CurrentProduct = value;
+                onPropertyChanged(nameof(CurrentProduct));
+            }
+        }
+        public string CurrentMessage
+        {
+            get => m_CurrentMessage;
+            set
+            {
+                m_CurrentMessage = value;
+                onPropertyChanged(nameof(CurrentMessage));
+            }
+        }
+        public Command AddSampleProduct
+        {
+            get; private set;
+        }
+        public Command RemoveSampleProduct
+        {
+            get; private set;
+        }
+        public Command UpdateSampleProduct
+        {
+            get; private set;
+        }
+
+        public DataContext dataContext
+        {
+            get => m_dataContext;
+            set
+            {
+                m_dataContext = value;
+                Products = new ObservableCollection<MyProduct>(value.products);
+            }
+        }
+
+        private void onPropertyChanged(string propertName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertName));
+        }
     }
 }
